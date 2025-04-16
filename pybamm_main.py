e6c05c0cb277606ea3925e06024749ddce4127a3
unknown <somondal@uw.edu>
2025-04-16 01:39:16 -0700

    Added all python codes

---
+++ pybamm_main.py
@@ -0,0 +1,95 @@
+import pybamm 
+import streamlit as st
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def generate_experiment(single_line, line_arr):
+    print("Single line")
+    if single_line[0]=='Charge' or single_line[0]=='Discharge':
+        if single_line[1]== 'Current':
+            if single_line[3]== 'Voltage':
+                line= f"{single_line[0]} at {single_line[2]} A for {single_line[6]} seconds or until {single_line[5]} V"
+            if single_line[3]== 'SOC':
+                line= f"{single_line[0]} at {single_line[2]} A for {single_line[6]} seconds or until {single_line[5]} SOC"
+        if single_line[1]== 'Power':
+            if single_line[3]== 'Voltage':
+                line= f"{single_line[0]} at {single_line[2]} W for {single_line[6]} seconds or until {single_line[5]} V"
+            if single_line[3]== 'SOC':
+                line= f"{single_line[0]} at {single_line[2]} W for {single_line[6]} seconds or until {single_line[5]} SOC"
+    if single_line[0]=='Rest':
+        line= f"{single_line[0]} for {single_line[6]} seconds"
+    print(line)
+    line_arr.append(line)
+    return line_arr
+
+def generate_model(selections, t, c, soc):
+    print(selections)
+    line_arr=[]
+    for i in range(len(selections)):
+        line_arr= generate_experiment(selections[i], line_arr)
+        print(line_arr)
+
+    model = pybamm.lithium_ion.DFN()
+    param = model.default_parameter_values
+
+# Set custom cell capacity (e.g., 3.2 Ah)
+    param.update({
+        f"Nominal cell capacity [A.h]": c,
+        f'Upper voltage cut-off [V]': 4.5,
+        f'Lower voltage cut-off [V]': 3.0,
+        f"Initial temperature [K]": t,
+    })
+    experiment = pybamm.Experiment(line_arr)
+        # [
+        #     "Discharge at C/10 for 10 hours or until 3.3 V",
+        #     "Rest for 1 hour",
+        #     "Charge at 1 A until 4.1 V",
+        #     "Hold at 4.1 V until 50 mA",
+        #     "Rest for 1 hour",
+        # ]
+    print("SOC=", soc)
+    sim = pybamm.Simulation(model, experiment=experiment)
+    sim.solve(initial_soc=soc)
+    solution = sim.solution
+    save_file= solution.save("dummy.pkl")
+    time = solution["Time [h]"].entries
+    voltage = solution["Voltage [V]"].entries
+    current = solution["Current [A]"].entries
+    dis_cap= solution["Discharge capacity [A.h]"].entries
+    
+    temperature= solution["Cell temperature [K]"].entries
+    resistance= solution["Resistance [Ohm]"].entries
+
+    # Plot
+    fig = make_subplots(rows=2, cols=2, subplot_titles=(
+        "Voltage(V)",  "Cell Temperature(K)", "Current(A)", "Discharge Capacity(Ah)"))
+
+    # Plot Voltage
+    fig.add_trace(go.Scatter(x=time, y=voltage, name="Voltage", line=dict(color='blue')),
+                row=1, col=1)
+
+
+    # Plot Temperature
+    fig.add_trace(go.Scatter(x=time, y=temperature, name="Temperature", line=dict(color='red')),
+                row=1, col=2)
+
+    # Plot Current
+    fig.add_trace(go.Scatter(x=time, y=current, name="Current", line=dict(color='orange')),
+                row=2, col=1)
+    
+    fig.add_trace(go.Scatter(x=time, y=dis_cap, name="Discharge Capacity", line=dict(color='pink')),
+                row=2, col=2)
+    
+
+
+    # Update layout
+    fig.update_layout(
+        height=1200, width=1000,
+        xaxis_title= "Time (hrs)",
+        title_text="Battery Simulation Summary",
+        showlegend=False
+    )
+    fig.update_xaxes(matches='x')
+
+    return fig
+
---
+++ pybamm_simulator.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import pybamm 
+import pybamm_main
+
+st.set_page_config(layout="wide")
+st.title("Select Battery Cycler Procedure")
+
+columns_main = ["Mode", "Type", "Value", "End Mode", "End operator", "End Value", "Maximum Time (s)"]
+options_mode = ["Charge", "Discharge", "Rest", "Waveform"]
+options_type = ['Current', 'Power']
+options_end_type = ['Voltage', 'Current']
+options_end_sign = ['>=', '<=', '=', '<', '>']
+
+# Initialize session state
+if "row_ids" not in st.session_state:
+    st.session_state.row_ids = [0]  # Start with one row (ID = 0)
+    st.session_state.next_id = 1    # To keep unique keys for new rows
+
+
+if "hold_confirm" not in st.session_state:
+    st.session_state.hold_confirm=0
+
+if "c_rate" not in st.session_state:
+    st.session_state.c_rate=3.2
+
+if "temp" not in st.session_state:
+    st.session_state.temp=298
+
+value = 0.0
+
+# Function to render a row
+def create_row(row_id):
+    cols = st.columns(len(columns_main) + 1)
+    selected0 = cols[0].selectbox(columns_main[0], options=options_mode, key=f'row{row_id}_0')
+    selected1 = cols[1].selectbox(columns_main[1], options=options_type, key=f'row{row_id}_1')
+    selected2 = cols[2].number_input(columns_main[2], value=value, key=f'row{row_id}_2')
+    selected3 = cols[3].selectbox(columns_main[3], options=options_end_type, key=f'row{row_id}_3')
+    selected4 = cols[4].selectbox(columns_main[4], options=options_end_sign, key=f'row{row_id}_4')
+    selected5 = cols[5].number_input(columns_main[5], value=value, key=f'row{row_id}_5')
+    selected6 = cols[6].number_input(columns_main[6], value=value, key=f'row{row_id}_6')
+
+    # Delete button
+    if cols[7].button("Delete", key=f'delete_{row_id}'):
+        st.session_state.row_ids.remove(row_id)
+        for i in range(7):
+            st.session_state.pop(f'row{row_id}_{i}', None)
+        st.rerun()
+
+    return [selected0, selected1, selected2, selected3, selected4, selected5, selected6]
+
+# Create rows dynamically
+st.session_state.selections = []
+for row_id in st.session_state.row_ids:
+    st.session_state.selections.append(create_row(row_id))
+
+temperature= st.number_input("Temperature", value=298.3, key='tem', label_visibility="visible")
+c_rate= st.number_input("C-rate", value=3.2, key='ah', label_visibility="visible")
+s_o_c= st.number_input("Initial SOC in fraction", value=0.5, key='soc', label_visibility="visible")
+
+# Add row button
+if st.button("+ Add row"):
+    new_id = st.session_state.next_id
+    st.session_state.row_ids.append(new_id)
+    st.session_state.next_id += 1
+    st.rerun()
+
+# Confirm button
+if st.button("Confirm"):
+    st.write("You selected:")
+    for i, row_id in enumerate(st.session_state.row_ids):
+        row = [st.session_state.get(f'row{row_id}_{j}') for j in range(7)]
+        st.write(f"Row {i+1}: {row}")
+    st.session_state.hold_confirm=1
+
+if st.session_state.hold_confirm==1:
+    if st.button("Generate plot and data"):
+        print("Selecetions", st.session_state.selections)
+        figure= pybamm_main.generate_model(st.session_state.selections, temperature, c_rate, s_o_c)
+        st.plotly_chart(figure)
+
+        with open("dummy.pkl", "rb") as f:
+            pkl_bytes = f.read()
+        st.download_button(
+            label="Download Pickle File",
+            data=pkl_bytes,
+            file_name="dummy.pkl",
+            mime="application/octet-stream"
+        )
